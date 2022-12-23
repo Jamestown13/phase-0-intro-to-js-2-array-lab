e3ac19411e7b4bedb51a1be5d0f42b1dee808a1c
Jamestown13 <118594452+Jamestown13@users.noreply.github.com>
2022-12-23 15:56:35 -0700

    commit tree 29cb4e2f55d7302b2b0f0549ab2c1b87a4e57179

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+const cats = ["Milo","Otis", "Garfield",]
+function destructivelyAppendCat (name){
+    cats.push(name)
+}function destructivelyPrependCat (name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat (name) {
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat (name){
+    cats.shift(name)
+}
+function appendCat (name){
+    var name_1= [...cats,name];
+  return name_1
+}
+function prependCat (name){
+    var name_2= [name, ...cats]
+    return name_2
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+  }
+function removeFirstCat() {
+    return cats.slice(1)
+}
